f2614afb75cce9e392a5abf00bc4b2b047be91ea
ITpassport <103945900+ITpassport@users.noreply.github.com>
2023-07-03 05:27:44 +0000

    aaa

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,8 @@
       <h5 class="card-title">各種入力</h5>
       <ul>
         <li><NuxtLink to ="pages/home">複合仕分入力</NuxtLink></li>
-        <li>元帳入力</li>
-        <li>通帳入力</li>
+        <li><NuxtLink to ="a">元帳入力</NuxtLink></li>
+        <li><NuxtLink to ="to">通帳入力</NuxtLink></li>
       </ul>
     </div>
   </div>
@@ -20,11 +20,16 @@
       <div class="col-4">
       <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
       <li><NuxtLink to ="a">出納表</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
+      <li><NuxtLink to ="a">仕分検索</NuxtLink></li>
+      <li><NuxtLink to ="a">合計残高計算書</NuxtLink></li>
+      <li><NuxtLink to ="a">補助検索合計残高表</NuxtLink></li>
+      <li><NuxtLink to ="a">消費税課税区分別集計書</NuxtLink></li>
+      <li><NuxtLink to ="a">総勘定元帳</NuxtLink></li>
+      <li><NuxtLink to ="a">補助元帳</NuxtLink></li>
+      <li><NuxtLink to ="a">月次比較財務書表</NuxtLink></li>
+      <li><NuxtLink to ="a">個人出納表</NuxtLink></li>
+      <li><NuxtLink to ="a">経費精算書</NuxtLink></li>
+      <li><NuxtLink to ="a">経費生産承認</NuxtLink></li>
     </div>
   </ul>
     </div>
@@ -35,13 +40,11 @@
       <h5 class="card-title">年次決算</h5>
       <ul class="list-inline">
       <div class="col-4">
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
-      <li><NuxtLink to ="a">出納表</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
-      <li><NuxtLink to ="a">仕分帳</NuxtLink></li>
+      <li><NuxtLink to ="a">期末整理表</NuxtLink></li>
+      <li><NuxtLink to ="a">年次繰越</NuxtLink></li>
+      <li><NuxtLink to ="a">決算報告書表示設定</NuxtLink></li>
+      <li><NuxtLink to ="a">変動利用設定</NuxtLink></li>
+      <li><NuxtLink to ="a">決算報告書</NuxtLink></li>
     </div>
   </ul>
     </div>
